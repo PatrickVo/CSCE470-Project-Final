bd0cdc61b4ba08228f7f5de24503e6e1062211d7
Patrick Vo <dragon.flare@hotmail.com>
2014-11-09 17:50:28 -0600

    Tkinter
    Having problems changing window and with the scroll bar but will change
    later. What i have right now

--- APP/yelpPD.py
+++ APP/yelpPD.py
@@ -5,9 +5,42 @@ window  = Tk()
 window.title("YelpPD")
 window.geometry("700x700")
 
+'''
 app = Frame(window)
 app.grid()
 label = Label(app, text = "YelpPd")
 label.grid()
+'''
 
+windowmode = 'list'
+
+def toreview():
+        print "it came here"
+        windowmode = 'write'
+
+def callback():
+        print "click!"
+
+w = Label(window, text="This is YelpPD")
+w.pack()
+if(windowmode == 'list'):
+    scrollbar = Scrollbar(window)
+    scrollbar.pack(side=RIGHT, fill=Y)    
+    listbox = Listbox(window, yscrollcommand=scrollbar.set)
+    for i in range(10):
+        listbox.insert(END, str(i))
+    listbox.pack(side=LEFT, fill=BOTH)    
+    scrollbar.config(command=listbox.yview)
+    reviewbutton = Button(window, text="Write Review",command=toreview)
+    reviewbutton.pack()
+    
+
+if(windowmode == 'write'):
+    T = Text(window, height=5, width=80)
+    T.pack()
+    T.insert(END, "Insert Review here")    
+    b = Button(window, text="Sumbit Review", command=callback)
+    b.pack()
+    
+window.update()
 window.mainloop()
