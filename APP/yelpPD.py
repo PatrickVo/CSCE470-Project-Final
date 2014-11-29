8224d353e2be071134378ff24eecc89c96fac593
Patrick Vo <dragon.flare@hotmail.com>
2014-11-29 14:25:04 -0600

    Updated GUI
    It can no change windows but having problems getting data from it

--- APP/yelpPD.py
+++ APP/yelpPD.py
@@ -1,67 +1,113 @@
 from Tkinter import *
 from PIL import Image, ImageTk
-
-window  = Tk()
-
-window.title("YelpPD")
-window.geometry("700x700")
-
-'''
-app = Frame(window)
-app.grid()
-label = Label(app, text = "YelpPd")
-label.grid()
-'''
-
-#windowmode = 'write'
-#windowmode = 'list'
-windowmode = 'finish'
-
-def toreview():
-        windowmode = 'write'      
-
-def callback():
-        print "click!"
+from Tkinter import Tk, RIGHT, BOTH, RAISED
+from ttk import Frame, Button, Style
 
 
-if(windowmode == 'list'):
-        w = Label(window, text="This is YelpPD")
+
+        
+class display1(Frame): 
+    
+    def __init__(self, parent):
+        Frame.__init__(self, parent)   
+         
+        self.parent = parent
+        
+        self.initUI()
+        
+    def initUI(self):
+
+        w = Label(self, text="This is YelpPD")
         w.pack()  
-        scrollbar = Scrollbar(window)
-        scrollbar.pack(side=RIGHT, fill=Y)    
-        listbox = Listbox(window, yscrollcommand=scrollbar.set)
-        for i in range(10):
+        listbox = Listbox(self, height=10)
+        for i in range(100):
             listbox.insert(END, "Restaurant #"+str(i))
-        listbox.pack(side=LEFT, fill=BOTH)    
+        scrollbar = Scrollbar(self,orient=VERTICAL)
+        scrollbar.configure(command=listbox.yview)
+        listbox.configure(yscrollcommand=scrollbar.set)
+        scrollbar.pack(side=LEFT, fill=Y) 
+        listbox.pack(side=LEFT, fill=Y)    
+        
         scrollbar.config(command=listbox.yview)    
-        reviewbutton = Button(window, text="Write a Review",command=toreview) 
-        reviewbutton.pack()    
-
-if(windowmode == 'write'):
-        w = Label(window, text="Write a Review")
+        reviewbutton = Button(self, text="Write a Review",command=self.toreview) 
+        reviewbutton.pack(side=BOTTOM, fill=X)   
+        self.pack()
+        
+    def toreview(self): 
+        self.pack_forget()   
+        display2(master)
+        
+class display2(Frame): 
+       
+    def __init__(self, parent):
+        Frame.__init__(self, parent)   
+         
+        self.parent = parent
+        
+        self.initUI()
+        
+    def initUI(self):
+        w = Label(self, text="Write a Review")
+        
         w.pack()          
-        T = Text(window, height=5, width=80)
-        T.pack()
-        Textbox = "Insert Review here"
-        T.insert(END, Textbox) 
-        b = Button(window, text="Submit Review", command=callback)
-        b.pack()     
+        review_text = StringVar() 
+    
+        e = Entry(self, textvariable=review_text)
+        review_text.set("Insert Review Here")
+    
+        e.pack(side=TOP, fill=BOTH)
+        b = Button(self, text="Submit Review",command=self.tostars)
+        b.pack(side=BOTTOM, fill=BOTH)
+        self.pack()
+    
+    def tostars(self): 
+        self.pack_forget()          
+        display3(master)
 
-if(windowmode == 'finish'):
-        w = Label(window, text="The Predicted Star Value")
+class display3(Frame):
+     
+    def __init__(self, parent):
+        Frame.__init__(self, parent)   
+         
+        self.parent = parent
+        
+        self.initUI()
+        
+    def initUI(self):
+        w = Label(self, text="The Predicted Star Value")
         w.pack() 
         image = Image.open("stars_3.png")
         photo = ImageTk.PhotoImage(image) 
-        label = Label(image=photo)
+        label = Label(self,image=photo)
         label.image = photo 
         label.pack()          
-        T = Text(window, height=5, width=80)
+        T = Text(self, height=5, width=80)
         T.pack()
-        Textbox = "Sumbited Review"
+        Textbox = 'sumbited text'
         T.insert(END, Textbox)         
-        b1 = Button(window, text="Write another review", command=callback)
-        b2 = Button(window, text="Change Restaurant", command=callback)
+        b1 = Button(self, text="Write another review", command=self.toreview)
+        b2 = Button(self, text="Change Restaurant", command=self.tolist)
         b1.pack()             
         b2.pack()
+        self.pack()
+        
+    def toreview(self): 
+        self.pack_forget()   
+        display2(master)
+    
+    def tolist(self): 
+        self.pack_forget()   
+        display1(master)  
 
-window.mainloop()
+def main():
+
+    global master,review_text,listbox,text
+
+    master  = Tk()
+    master.title("YelpPD")
+    master.geometry("700x700")    
+    display1(master)    
+    mainloop()
+    
+if __name__ == '__main__':
+    main()  
