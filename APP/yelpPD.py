60d01f7a6d7c0d49aa601994368008357b0539a4
Patrick Vo <dragon.flare@hotmail.com>
2014-11-30 22:00:55 -0600

    Fixed problem and list come from yelp json files

--- APP/yelpPD.py
+++ APP/yelpPD.py
@@ -2,10 +2,13 @@ from Tkinter import *
 from PIL import Image, ImageTk
 from Tkinter import Tk, RIGHT, BOTH, RAISED
 from ttk import Frame, Button, Style
+import json #or cjson
+import re
 
-
-
-        
+business_list = []
+master = ''
+submitted_text = ''
+      
 class display1(Frame): 
     
     def __init__(self, parent):
@@ -20,8 +23,8 @@ class display1(Frame):
         w = Label(self, text="This is YelpPD")
         w.pack()  
         listbox = Listbox(self, height=10)
-        for i in range(100):
-            listbox.insert(END, "Restaurant #"+str(i))
+        for i in business_list:
+            listbox.insert(END, i['name'])
         scrollbar = Scrollbar(self,orient=VERTICAL)
         scrollbar.configure(command=listbox.yview)
         listbox.configure(yscrollcommand=scrollbar.set)
@@ -33,8 +36,8 @@ class display1(Frame):
         reviewbutton.pack(side=BOTTOM, fill=X)   
         self.pack()
         
-    def toreview(self): 
-        self.pack_forget()   
+    def toreview(self):         
+        self.pack_forget()           
         display2(master)
         
 class display2(Frame): 
@@ -47,20 +50,19 @@ class display2(Frame):
         self.initUI()
         
     def initUI(self):
-        w = Label(self, text="Write a Review")
-        
+        w = Label(self, text="Write a Review")        
         w.pack()          
-        review_text = StringVar() 
-    
+        review_text = StringVar()     
         e = Entry(self, textvariable=review_text)
-        review_text.set("Insert Review Here")
-    
+        review_text.set("Insert Review Here")    
         e.pack(side=TOP, fill=BOTH)
-        b = Button(self, text="Submit Review",command=self.tostars)
+        b = Button(self, text="Submit Review",command=lambda: self.tostars(e.get()))
         b.pack(side=BOTTOM, fill=BOTH)
         self.pack()
     
-    def tostars(self): 
+    def tostars(self,text): 
+        global submitted_text
+        submitted_text = text
         self.pack_forget()          
         display3(master)
 
@@ -83,7 +85,7 @@ class display3(Frame):
         label.pack()          
         T = Text(self, height=5, width=80)
         T.pack()
-        Textbox = 'sumbited text'
+        Textbox = submitted_text
         T.insert(END, Textbox)         
         b1 = Button(self, text="Write another review", command=self.toreview)
         b2 = Button(self, text="Change Restaurant", command=self.tolist)
@@ -99,13 +101,19 @@ class display3(Frame):
         self.pack_forget()   
         display1(master)  
 
+def loadbusiness():
+    infilename='yelp_academic_dataset_business.json'
+    f=open(infilename,'r')
+    for line in f:
+        business_list.append(json.loads(line))
+   
+        
+        
 def main():
-
-    global master,review_text,listbox,text
-
     master  = Tk()
     master.title("YelpPD")
-    master.geometry("700x700")    
+    master.geometry("700x700")  
+    loadbusiness()  
     display1(master)    
     mainloop()
     
